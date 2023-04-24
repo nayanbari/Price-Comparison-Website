df4be6638080a469135686dbcfebd246639ec1be
nayanbari <nayanbari89@gmail.com>
2023-04-25 03:52:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Home from './pages/Home';
+import SignUp from './pages/SignUp';
+import NavigationBar from './components/NavigationBar';
+import Track from './pages/Track';
+import PriceComparison from './pages/PriceComparison';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <body className="h-full">
+      <NavigationBar/>
+      <Routes>
+      <Route path="/home" exact element={<Home />} />
+      <Route path="/" exact element={<SignUp />} />
+      <Route path="/track" exact element={<Track />} />
+      <Route path="/comparison" exact element={<PriceComparison />} />
+      </Routes>
+    </body>
+    </Router>
   );
 }
 
